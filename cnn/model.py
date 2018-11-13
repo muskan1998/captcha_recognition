2eb2ab22964afc92edeb2f0074ed5bdd8ca833ee
Muskan Bansal <muskanbansal98@gmail.com>
2018-11-13 10:32:14 +0530

    Update model.py

--- cnn/model.py
+++ cnn/model.py
@@ -1,16 +1,63 @@
-import keras
-from keras.models import Sequential
-from keras.layers.core import Flatten, Dense, Dropout
-from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
-from keras.optimizers import SGD, Adam, Adamax
-from keras.preprocessing.image import ImageDataGenerator
-from keras.layers.convolutional import Conv2D, MaxPooling2D
 import cv2
+import pickle
+import os.path
 import numpy as np
-import h5py
+from imutils import paths
+from sklearn.preprocessing import LabelBinarizer
+from sklearn.model_selection import train_test_split
+from keras.models import Sequential
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+from keras.layers.core import Flatten, Dense
+from helpers import resize_to_fit
 
-#'vgg16_weights.h5''vgg16_weights.h5'
 
+LETTER_IMAGES_FOLDER = "image2"
+MODEL_FILENAME = "model.hdf5"
+MODEL_LABELS_FILENAME = "labels.dat"
+
+
+# initialize the data and labels
+data = []
+labels = []
+
+# loop over the input images
+for image_file in paths.list_images(LETTER_IMAGES_FOLDER):
+    # Load the image and convert it to grayscale
+    image = cv2.imread(image_file)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Resize the letter so it fits in a 20x20 pixel box
+    image = cv2.resize(image, (20,20))
+
+    # Add a third channel dimension to the image to make Keras happy
+    image = np.expand_dims(image, axis=2)
+
+    # Grab the name of the letter based on the folder it was in
+    label = image_file.split(os.path.sep)[-2]
+
+    # Add the letter image and it's label to our training data
+    data.append(image)
+    labels.append(label)
+
+
+# scale the raw pixel intensities to the range [0, 1] (this improves training)
+data = np.array(data, dtype="float") / 255.0
+labels = np.array(labels)
+
+# Split the training data into separate train and test sets
+(X_train, X_test, Y_train, Y_test) = train_test_split(data, labels, test_size=0.25, random_state=0)
+
+# Convert the labels (letters) into one-hot encodings that Keras can work with
+lb = LabelBinarizer().fit(Y_train)
+Y_train = lb.transform(Y_train)
+Y_test = lb.transform(Y_test)
+
+# Save the mapping from labels to one-hot encodings.
+# We'll need this later when we use the model to decode what it's predictions mean
+with open(MODEL_LABELS_FILENAME, "wb") as f:
+    pickle.dump(lb, f)
+
+# Build the neural network!
 model = Sequential()
 
 # First convolutional layer with max pooling
@@ -26,47 +73,13 @@ model.add(Flatten())
 model.add(Dense(500, activation="relu"))
 
 # Output layer with 32 nodes (one for each possible letter/number we predict)
-model.add(Dense(32, activation="softmax"))
+model.add(Dense(27, activation="softmax"))
 
+# Ask Keras to build the TensorFlow model behind the scenes
+model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
 
+# Train the neural network
+model.fit(X_train, Y_train, validation_data=(X_test, Y_test), batch_size=2, epochs=7, verbose=1)
 
-for layer in model.layers:
-    layer.trainable = False
-
-
-model.summary()
-#new_model.load_weights('my_model_weights.h5')
-
-
-
-train_datagen = ImageDataGenerator(
-        shear_range=0.2,
-        zoom_range=0.2,
-        horizontal_flip=True)
-
-test_datagen = ImageDataGenerator()
-                                  
-nb_train_samples = 1202
-nb_validation_samples = 771
-epochs = 1
-batch_size = 8
-
-train_generator = train_datagen.flow_from_directory(
-        'image2',  
-        batch_size=batch_size,
-        shuffle=True,
-        target_size=(20,20),
-        class_mode='categorical')  
-
-
-
-model.compile(loss='categorical_crossentropy',
-              optimizer=Adam(lr=0.005, beta_1=0.9, beta_2=0.999, epsilon=None, decay=1e-9, amsgrad=False),
-              metrics=['accuracy'])
-
-
-
-
-model.save_weights('my_model_weights2.h5')
-
-
+# Save the trained model to disk
+model.save(MODEL_FILENAME)
